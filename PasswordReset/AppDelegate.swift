7e74a2f91791e7585717f8dc52e81fa4daf2a211
Ramy Atalla <ramywadia@gmail.com>
2022-06-11 16:13:51 +0200

    feat: create new password project

---
+++ PasswordReset/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PasswordReset
+//
+//  Created by Ramy Atalla on 2022-06-11.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+}
+
+
